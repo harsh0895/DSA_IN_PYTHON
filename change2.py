5da15ed4c44375533f74c1c6324221d43203dff8
puneet-complere <puneet.taneja@complereinfosystem.com>
2024-10-04 13:54:07 +0530

    luhdfsl

--- change2.py
+++ change2.py
@@ -1,1 +1,12 @@
+import geocoder
 
+def get_state(lat, long):  
+    g = geocoder.osm([lat, long], method='reverse')    
+    return g.state
+
+lat = 32.5467483
+long = -86.5315257
+
+
+state = get_state(lat, long)
+print(state)
